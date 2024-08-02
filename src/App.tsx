e80f071e808c03ad20ba73b33a3ee0c13f9eaa1a
venkataswamybudige <82580597+venkataswamybudige@users.noreply.github.com>
2024-08-02 21:47:39 +0530

    added carousel to the home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { ExportTopBooks } from './layouts/HomePage/ExploreTopBooks';
+import { Carousel } from './layouts/HomePage/Carousel';
 
 function App() {
   return (
     <div>
     <Navbar></Navbar>
     <ExportTopBooks></ExportTopBooks>
+    <Carousel></Carousel>
     </div>
     
     
