976976684e4470ac8c98388009fef0da477aa46e
venkataswamybudige <82580597+venkataswamybudige@users.noreply.github.com>
2024-08-02 19:48:31 +0530

    ExploreTopBooks component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { ExportTopBooks } from './layouts/HomePage/ExploreTopBooks';
 
 function App() {
   return (
+    <div>
     <Navbar></Navbar>
+    <ExportTopBooks></ExportTopBooks>
+    </div>
+    
     
   );
 }
