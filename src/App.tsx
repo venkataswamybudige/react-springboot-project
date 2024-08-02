4e6858251ec38322a7687e567e59b29199346e38
venkataswamybudige <82580597+venkataswamybudige@users.noreply.github.com>
2024-08-02 19:33:21 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+
+function App() {
+  return (
+    <Navbar></Navbar>
+    
+  );
+}
+
+export default App;
